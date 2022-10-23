0e1cd0c9fedd370c42bfc44c3243f0e66a756c18
kubraucar1 <kubraucar02@outlook.com>
2022-10-23 11:20:32 +0300

    comment

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -20,16 +20,16 @@ def twitter_func():
         sign_in=driver.find_element(By.XPATH,"//div/div/div/div[2]/main/div/div/div/div/div/div[3]/div[5]/a")
         sign_in.click()
 
-        #Get username field
-        username_input=driver.find_element(By.XPATH,"//input[@autocomplete='username']")
-        username_input.send_keys(username) #Enter your valid username or email or phone number
+        
+        username_input=driver.find_element(By.XPATH,"//input[@autocomplete='username']")  # Get username field
+        username_input.send_keys(username)   #Enter your valid username or email or phone number
 
         next_btn=driver.find_element(By.XPATH,"//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div")
         next_btn.click()
 
         driver.implicitly_wait(5)
         password_input=driver.find_element(By.XPATH,"//input[@autocomplete='current-password']")
-        password_input.send_keys(password) #Enter your valid password
+        password_input.send_keys(password)  #Enter your valid password
 
         login_btn=driver.find_element(By.XPATH,"//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div")
         login_btn.click()
