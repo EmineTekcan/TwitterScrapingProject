964567966df0c6a204e7e9398e611e6605b66f2d
Esmanur Deli <dlesmanur@gmail.com>
2022-12-22 13:04:41 +0300

    Functions have been added to find the most positive and negative tweets

--- Search/search.py
+++ Search/search.py
@@ -32,8 +32,61 @@ def search_tw(data_words,data_tw):
 
 data_negative = search_tw(column_name_negative,data_tw)
 NegativeSentence = pd.DataFrame(data_negative,columns=["Tweets"])
-NegativeSentence.to_csv("NegativeSentence.csv")
+NegativeSentence.to_csv("Search/NegativeSentence.csv")
 
 data_positive = search_tw(column_name_positive,data_tw)
-PositiveSentence = pd.DataFrame(data_positive,columns="Tweets")
-PositiveSentence.to_csv("PositiveSentence.csv")
+PositiveSentence = pd.DataFrame(data_positive,columns=["Tweets"])
+PositiveSentence.to_csv("Search/PositiveSentence.csv")
+
+
+#Most positive and negative tweets
+
+def most(data_tw,data_words):
+    liste = []
+    most_num = []
+    most_tweet = []
+    for x in range(0,len(data_words)):
+        count = 0
+        for y in range(0, len(data_tw)):
+            word = str(data_words[x]).lower().strip()
+            tweet = data_tw[y]
+            tweet = str(tweet).lower().split(" ")
+            for z in tweet:
+                if str(z) != str(word):
+                    continue
+                else:
+                    count+=1
+                    most_num.append(count)
+
+    for x in range(0,len(data_words)):
+        count = 0
+        for y in range(0, len(data_tw)):
+            word = str(data_words[x]).lower().strip()
+            tweet = data_tw[y]
+            tweet = str(tweet).lower().split(" ")
+            for z in tweet:
+                if str(z) != str(word):
+                    continue
+                else:
+                    count+=1
+                    if count == max(most_num) and len(most_tweet) == 0:
+                        most_tweet.append(data_tw[y])
+                       
+    return most_tweet
+
+
+data_positive_tweets = pd.read_csv("Search/PositiveSentence.csv")
+data_positive_column = data_positive_tweets["Tweets"]
+most_positive_tweet = most(data_positive_column,column_name_positive)  
+
+MostPositiveSentence = pd.DataFrame(most_positive_tweet,columns=["Tweets"])
+MostPositiveSentence.to_csv("Search/MostPositiveSentence.csv")
+
+#######
+
+data_negative_tweets = pd.read_csv("Search/NegativeSentence.csv")
+data_negative_column = data_negative_tweets["Tweets"]
+most_negative_tweet = most(data_negative_column,column_name_negative)  
+
+MostNegativeSentence = pd.DataFrame(most_negative_tweet,columns=["Tweets"])
+MostNegativeSentence.to_csv("Search/MostNegativeSentence.csv")
